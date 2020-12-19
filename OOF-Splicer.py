9fabc99594e64b9c7d160c0cf7673cbb87a257d9
gammareflector <56936086+gammareflector@users.noreply.github.com>
2020-12-19 00:19:23 -0600

    Add files via upload
    Added drag and drop support for the splicer, removed text prompts, added comments, cleaned up code, reduced file size. Updated readme as well.

--- OOF-Splicer.py
+++ OOF-Splicer.py
@@ -1,36 +1,34 @@
 import pickle
+import sys
+import os
 
-mb=8192000
+mb=8192000 # Size to split files into
 
 class out:
     def __init__(self, name):
         self.name=name
 
-def make(start, end, i, data):
+def make(start, end, i, data): # Function for creating split files
     print('Generating OOF '+str(i+1)+'...')
+    
     if end > len(data):
         end=len(data)
     f=open('S'+str(i+1)+'.OOF','wb')
+    
     for i in range(start, end):
         f.write(bytes([data[i]]))
     f.close()
+    
 if __name__ == '__main__':
-    valid=False
-    while not valid:
-        name=str(input('FILE NAME:'))
-        try:
-            index=out(name)
-            file=open(name, 'rb')
-            data=pickle.dumps(file.read())
-            file.close()
-            form=open('S0.OOF', 'wb')
-            form.write(pickle.dumps(index))
-            form.close()
-            num=int(len(data)/mb)+1
-            valid=True
-            print('Generating '+str(num+1)+ ' OOF\'s...')
-            print('Generating OOF 0...')
-            for i in range(0,num):
-                make(i*mb, i*mb+mb, i, data)
-        except:
-            print('INVALID FILE NAME!\n')
+        index=out(os.path.basename(sys.argv[1])) # Get filename from full path
+        file=open(sys.argv[1], 'rb')
+        data=pickle.dumps(file.read())
+        file.close()
+        form=open('S0.OOF', 'wb')
+        form.write(pickle.dumps(index)) # Dump filename to S0.OOF
+        form.close()
+        num=int(len(data)/mb)+1
+        print('Generating '+str(num+1)+ ' OOF\'s...')
+        print('Generating OOF 0...')
+        for i in range(0,num):
+            make(i*mb, i*mb+mb, i, data) # Create split files
--- README.md
+++ README.md
@@ -5,5 +5,4 @@
 - The Stitcher program will piece the smaller 8MB files back together
 
 ### Side notes
-- Please specify file name and extension for splicing ex: "bob.txt".
 - Place all the .OOF files in the same directory as the Stitcher and it will automatically work on running.
